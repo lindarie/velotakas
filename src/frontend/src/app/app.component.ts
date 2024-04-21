4efb694e31cdbebfe5e77652658964e775c4cc54
lindarie <rieksta.linda20@gmail.com>
2024-04-21 14:50:24 +0300

    Add Angular frontend to Spring Boot backend

---
+++ src/frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
+})
+
+export class AppComponent {
+  title = 'velotakas';
+}
