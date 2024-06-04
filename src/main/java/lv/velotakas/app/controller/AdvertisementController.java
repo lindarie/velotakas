ba6e95bd59376bb53269ada6edde1c1854a00664
Ralfs Laksa <ralfslaksa03@gmail.com>
2024-06-04 12:44:57 +0300

    put, delete ad

--- src/main/java/lv/velotakas/app/controller/AdvertisementController.java
+++ src/main/java/lv/velotakas/app/controller/AdvertisementController.java
@@ -2,17 +2,20 @@ package lv.velotakas.app.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import lv.velotakas.app.dto.request.advertisement.AdvertisementDTO;
+import lv.velotakas.app.dto.request.advertisement.AdvertisementUpdateDTO;
 import lv.velotakas.app.dto.response.advertisement.AdvertisementResponseDTO;
 import lv.velotakas.app.service.AdvertisementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/ad")
 public class AdvertisementController {
 
@@ -44,5 +47,27 @@ public class AdvertisementController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(advertisement);
+    }
+    @PutMapping("/{id}")
+    @Operation(summary = "Update a an advertisement")
+    ResponseEntity editAdvertisementById(@RequestBody AdvertisementUpdateDTO updateRequest,
+                                   @PathVariable Integer id) {
+        if (!adService.advertisementExistsById(id)) {
+            log.warn("Advertisement controller: Not found advertisement with ID " + id);
+
+            return ResponseEntity.notFound().build();
+        }
+        adService.updateAdvertisement(updateRequest, id);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a advertisement")
+    ResponseEntity<Void> deleteAdvertisement(@PathVariable Integer id) {
+        if (adService.advertisementExistsById(id)) {
+            adService.deleteAdvertisementById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
